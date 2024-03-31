6e85540b28dea87c1df94766db687b7487107418
tasmanit <63876838+tasmanit@users.noreply.github.com>
2024-04-01 03:02:11 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,19 @@ $('#tarifip').owlCarousel({
         }
     }
 });
+$('.itrsnoslide').owlCarousel({
+    margin:15,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
