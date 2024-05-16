1743d2770e831cc8472a058353ba9237b6008c29
tasmanit <63876838+tasmanit@users.noreply.github.com>
2024-05-16 23:53:05 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,3 +110,22 @@ $('.itrsnoslide').owlCarousel({
         }
     }
 });
+$('#company').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
